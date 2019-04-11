09158ec06d13548ffe78442316c0ae20c3d066fd
Winnerawan T <third1337@gmail.com>
2019-04-11 23:14:38 +0700

    fix news json

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class NewsController extends Controller
 {
     public function index() {
-        return response()->json([\App\News::take(4)->orderBy('tanggal', 'DESC')->get()]);
+        return response()->json(\App\News::take(4)->orderBy('tanggal', 'DESC')->get());
     }
 }
