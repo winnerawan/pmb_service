c116c5fcc89213efc1c33cc72dc95f8dbe6857fe
Winnerawan T <third1337@gmail.com>
2019-04-14 13:30:52 +0700

    update

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Sunra\PhpSimple\HtmlDomParser;
 
 class NewsController extends Controller
 {
@@ -15,5 +16,31 @@ class NewsController extends Controller
         $x->error = true;
         $x->message = "Congratulations! You have found an empty page! Go celebrate this great discovery by sending the admin a coffee";
         return response()->json($x);
+    }
+
+    public function news() {
+        
+        $data[] = array();
+        $link = "http://unipma.ac.id";
+        $html = HtmlDomParser::file_get_html($link, false, null, 0);
+        foreach($html->find('.img-thumbnail') as $i => $a0){
+            $style = $a0->style;
+            preg_match('/\(([^)]+)\)/', $style, $match);
+            $data[$i]['image'] = $match[1];
+            foreach($html->find('.portfolio-details a > h5') as $i => $item) {
+                $data[$i]['title'] = $item->innertext;
+                foreach($html->find('.portfolio-details a > a') as $x => $l) {
+                    $data[$x]['link'] = "http://unipma.ac.id/" . $l->href;
+                } 
+            }
+        }
+        $i=0;
+        foreach($data as $element) {
+        if(sizeof($element)<=1){
+                unset($data[$i]);
+            }
+            $i++;
+        }
+        echo json_encode($data);
     }
 }
